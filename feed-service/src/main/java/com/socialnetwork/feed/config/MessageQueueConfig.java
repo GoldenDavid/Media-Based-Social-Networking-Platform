def80365755fa86c693528d3a7c31b0e01578084
GoldenDavid <huylkt1@gmail.com>
2026-05-31 17:13:26 +0700

    feat(feed-service): Add Redis, RabbitMQ, and Security configs

---
+++ feed-service/src/main/java/com/socialnetwork/feed/config/MessageQueueConfig.java
@@ -0,0 +1,16 @@
+package com.socialnetwork.feed.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MessageQueueConfig {
+    public static final String AFTER_CREATE_POST_QUEUE = "after-create-post-queue";
+
+    @Bean
+    Queue afterCreatePostQueue() {
+        return QueueBuilder.durable(AFTER_CREATE_POST_QUEUE).build();
+    }
+}
---
+++ feed-service/src/main/java/com/socialnetwork/feed/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.socialnetwork.feed.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers(new AntPathRequestMatcher("/swagger-ui/**")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/api-docs/**")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/actuator/**")).permitAll()
+                .anyRequest().authenticated()
+            )
+            .csrf(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
+}
