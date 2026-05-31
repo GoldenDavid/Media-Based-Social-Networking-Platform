0aceda0308142fac15abdf721a770920880b77fb
GoldenDavid <huylkt1@gmail.com>
2026-05-31 17:13:27 +0700

    feat(feed-service): Migrate DynamicFeedController and PreComputedFeedController

---
+++ feed-service/src/main/java/com/socialnetwork/feed/controller/PreComputedFeedController.java
@@ -0,0 +1,35 @@
+package com.socialnetwork.feed.controller;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.socialnetwork.feed.dto.GetFeedResponse;
+import com.socialnetwork.feed.dto.UserPrincipal;
+import com.socialnetwork.feed.service.FeedService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@Slf4j
+@RequestMapping(path = "/precomputed-feeds")
+public class PreComputedFeedController {
+    private FeedService feedService;
+
+    public PreComputedFeedController(@Qualifier("precomputedFeedService") FeedService feedService) {
+        this.feedService = feedService;
+    }
+
+    @GetMapping()
+    public ResponseEntity<GetFeedResponse> getFeed(@RequestParam("page") int page,
+                                                   @RequestParam("limit") int limit, Authentication authentication) {
+        log.info("page={}, limit={}", page, limit);
+        UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
+        GetFeedResponse response = feedService.getFeed(userPrincipal, limit, page);
+        return ResponseEntity.ok().body(response);
+    }
+}
